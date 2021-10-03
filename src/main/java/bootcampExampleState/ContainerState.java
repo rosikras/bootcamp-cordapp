8f2c345595166804b4e9d64fa2229beeaa45c18b
Rositsa Krasteva <rositsa.krasteva@accenture.com>
2021-10-03 20:56:21 +0200

    Bootcamp: Solve State Tests

---
+++ src/main/java/bootcampExampleState/ContainerState.java
@@ -0,0 +1,66 @@
+package bootcampExampleState;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ContainerState implements ContractState {
+    private int windth;
+    private int height;
+    private int depth;
+    private String content;
+    private Party owner;
+    private Party carrier;
+
+    public ContainerState(int windth, int height, int depth, String content, Party owner, Party carrier) {
+        this.windth = windth;
+        this.height = height;
+        this.depth = depth;
+        this.content = content;
+        this.owner = owner;
+        this.carrier = carrier;
+    }
+
+    public int getWindth() {
+        return windth;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public Party getCarrier() {
+        return carrier;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return ImmutableList.of(owner, carrier);
+    }
+
+    public static void main (String [] args){
+
+        Party bagImporters = null;
+        Party bagCarrier = null;
+        ContainerState containerState = new ContainerState(2, 4, 2, "bags", bagImporters, bagCarrier);
+    }
+
+
+}
---
+++ src/main/java/bootcampExampleState/HouseState.java
@@ -0,0 +1,40 @@
+package bootcampExampleState;
+
+import com.google.common.collect.ImmutableList;
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class HouseState implements ContractState {
+
+    private String address;
+    private Party owner;
+
+    public HouseState(String address, Party owner) {
+        this.address = address;
+        this.owner = owner;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public Party getOwner(){
+        return this.owner;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        // The owners that will be notified when the house is created, modified or destroyed
+        return ImmutableList.of(owner);
+    }
+
+    public static void main(String [] args ){
+        Party rosi = null;
+        HouseState houseState = new HouseState("Donaufeld 54", rosi);
+    }
+}
